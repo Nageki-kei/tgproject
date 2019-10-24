8cdcd5ca8ddbce234b4e4f7f3bbfa161812e5f7d
IfnuAzis <azis.ifnu@gmail.com>
2019-10-24 10:24:32 +0700

    new

---
+++ projectquasar/src/layouts/MyLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="hhh lpR fff">
+
+    
+    <q-header elevated class="bg-primary text-white " height-hint="100">
+      <q-toolbar align="center">
+        <div class="absolute-center">
+        <q-toolbar-title >
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          DigiBit
+        </q-toolbar-title>
+        </div>
+        <q-space/>
+
+       <q-item clickable tag="a" target="" href="account">
+         <q-item-section>
+            <q-item-label>Account</q-item-label>
+          </q-item-section>
+       </q-item>
+
+      <q-item clickable tag="a" target="" href="account">
+         <q-item-section>
+            <q-item-label>Search</q-item-label>
+          </q-item-section>
+       </q-item>
+
+       <q-item dense flat round clickable @click="drawer = !drawer">
+         <q-item-section>
+            <q-item-label>Cart</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+          <q-icon name="shopping_cart"/>
+          </q-item-section>
+       </q-item>
+
+      <q-drawer v-model="drawer" overlay bordered content-class="bg-grey-3"  side="right" >
+      </q-drawer>
+
+
+      </q-toolbar>
+<!-- menu -->
+      <div class="column" style="height: 10px"/>
+      <q-tabs align="center" v-model="tab" inline-label class="bg-primary text-white shadow-2">
+        
+        <q-tab style="widht:250px" stretch flat label="HOME" @click="$router.replace('/home')" />
+      
+        <q-tab style="widht:250px" stretch flat label="SEMUA PRODUK" @click="$router.replace('/semuaproduk') "/>
+
+        <q-tab style="widht:250px" stretch flat label="HUBUNGI TG PROJECT" @click="$router.replace('/tg') "/>
+
+        <q-tab style="widht:250px" stretch flat label="TENTANG TG PROJECT" @click="$router.replace('/tentang') "/>
+
+      </q-tabs>
+    </q-header>
+<!--  -->
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+           
+          </q-avatar>
+        
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+       tab: 'mails',
+       drawer: false
+    }
+  },
+  methods: {
+   
+
+    }
+  }
+
+</script>
---
+++ projectquasar/src/pages/Index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="q-pa-md">
+   <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      infinite
+      autoplay
+      transition-prev="slide-right"
+      transition-next="slide-left"
+    >
+      <q-carousel-slide name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">First stop</div>
+          <div class="text-subtitle1">Mountains</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="second" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Second stop</div>
+          <div class="text-subtitle1">Famous City</div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="third" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Third stop</div>
+          <div class="text-subtitle1">Famous Bridge</div>
+        </div>
+      </q-carousel-slide>
+
+    </q-carousel>
+      <div class="column" style="height: 20px"/>
+        <!-- <div class="row justify-center">
+            <div class="col-auto">
+              <q-btn style="width: 350px"  push color="white" text-color="primary" label="Women Best Seller" />
+            </div>
+            <div class="col-3"/>
+            <div class="col-auto">
+              <q-btn style="width: 350px" push color="white" text-color="primary" label="Men Best Seller" />
+            </div>
+        </div> -->
+      <div class="column" style="height: 20px"/>
+  
+    <div class="q-gutter-md row justify-center">
+        <div class="col-3 ">
+          <q-card>
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" style="width: 100%">
+           <div class="absolute-bottom flex flex-center btn-center q-pa-xs"> 
+              <q-btn color="primary" label="sneakers" />
+            </div>
+          </q-img>
+          </q-card>
+        </div>
+        <div class="col-xs-1 q-col-gutter-xl"/>
+        <div class="col-3 ">
+          <q-card>
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" style="width: 100%">
+            <div class="absolute-bottom flex flex-center btn-center q-pa-xs">
+              <q-btn outline color="white" label="Apparel" />
+            </div>
+          </q-img>
+          </q-card>
+        </div>
+      
+        <div class="col-xs-1 q-col-gutter-x-xl"/>
+        <div class="col-3 ">
+          <q-card>
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" style="width: 100%">
+            <div class="absolute-bottom flex flex-center btn-center q-pa-xs">
+              <q-btn outline color="white" label="Accessories" />
+            </div>
+          </q-img>
+          </q-card>
+        </div>
+    </div>
+
+    <div class="column" style="height: 20px"/>
+
+
+
+  
+  </div>
+</template>
+ 
+
+<script>
+export default {
+   data () {
+    return {
+      slide: 'first'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+
+.my-card
+  width: 100%
+  max-width: 250px
+
+</style>
