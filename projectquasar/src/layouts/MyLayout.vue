22ef9c35922aae0bf0f208fd27517f51153b6b37
IfnuAzis <azis.ifnu@gmail.com>
2019-10-25 13:48:02 +0700

    clear for index

--- projectquasar/src/layouts/MyLayout.vue
+++ projectquasar/src/layouts/MyLayout.vue
@@ -73,7 +73,7 @@
      <q-separator color="blue-grey-3" inset />
      <div class="column">
        <div class="column" style="height: 70px"/>
-         <div class="row">
+         <div class="row" style="height: 180px">
             <div class="col-1 q-gutter-xl"/>
             <div class="col-3">
               <div class="text-left text-overline">TENTANG KAMI</div>
@@ -99,6 +99,34 @@
                <q-btn style="background: #283b39; color: white" label="Langganan" />
             </div>
          </div> 
+         <div class="row" style="height: 100px">
+          <div class="col-1 q-gutter-xl "/>
+          <div class="col-10">
+            <div class="column" style="100px">
+              <div class="col-1">
+                <div class="text-left text-overline">Kenali kami lebih baik dengan mengikuti pegangan media</div>
+                <div class="text-left text-overline">sosial kami!</div>
+              </div>
+            </div> 
+         </div>
+        </div>
+
+          <div class="column" style="240px">
+            <div class="row">
+              <div class="col-1 q-gutter-xl "/>
+              <div class="col1">
+                <div class="text-left text-overline">@TGProject</div>
+              </div>
+              <div class="col-10"/>
+              <div class="col-auto justify-end">
+                <q-img src="./statics/master.png" style="width:30px"/>
+                <q-img src="./statics/visa.png" style="width:30px"/>
+              </div>
+            </div>
+          </div>
+
+         </div>
+         
      </div>
     </q-footer>
 
@@ -120,3 +148,9 @@ export default {
   }
 
 </script>
+
+<style lang="sass" scoped>
+.visa
+  height: 50px
+  width:  80px
+</style>
